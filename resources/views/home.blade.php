231221a84aad93a68f04430f1d9a1b415065c3df
AlejandroVillalba <alejandrovillalba007@gmail.com>
2020-06-25 23:57:28 -0400

    correciones en el login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Tablero</div>
+                <div class="card-header">Carpinteria</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Bienvenido estas conectado
+                    Bienvenido {{ Auth::user()->name }} te haz conectado con exito
                 </div>
             </div>
         </div>
